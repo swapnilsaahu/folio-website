aff07c381c9c07ee493e57d8ceabce041056e36b
swapnilsaahu <swapnilvp846@gmail.com>
2025-06-15 03:47:00 +0530

    footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,8 +1,8 @@
 import { FaRegCopyright } from "react-icons/fa";
 const FooterComponent = () => {
     return (
-        <div className="flex justify-center h-[50vh] px-5 md:px-12 py-15">
-            <div className="bg-gray-200 px-5 py-5 w-screen flex flex-col justify-end">
+        <div className="flex justify-center px-5 md:px-12 py-15 items-center">
+            <div className="px-5 py-5 w-screen flex flex-col items-center">
                 <FaRegCopyright />
                 <p className="font-['kadwa']">Developed By</p>
                 <h2 className="font-['Just_Another_Hand'] text-xl">SWAPNIL SAHU</h2>
